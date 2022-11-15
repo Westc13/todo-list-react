c7f32213a3bed0585ad925daf4a60e14709f63f7
ChristineWest <christinewwwest@gmail.com>
2022-11-15 18:04:15 -0500

    set up HTML

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 import TodoList from "./TodoList";
 function App() {
-  return <TodoList />;
+  return (
+    <>
+      <TodoList />
+      <input type="text" />
+      <button>Add Task</button>
+      <button>Clear Finished Task</button>
+      <div>0 left todo</div>
+    </>
+  );
 }
 
 export default App;
