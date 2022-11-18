d31a521aebd9cf5a62e31f0d1cd310588581f462
ChristineWest <christinewwwest@gmail.com>
2022-11-18 10:55:32 -0500

    added useRef to get hold of the value from the HTML input field

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import TodoList from "./TodoList";
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, name: "Todo 1", complete: false },
-  ]);
+  const [todos, setTodos] = useState([]);
   /* object destructure - the todos in the array above represent every state, the setTodos is the function that updates those todos */
+  const todoNameRef = useRef();
+  function handleAddTodo(e) {
+    todoNameRef.current;
+  }
   return (
     <>
       <TodoList todos={todos} />
-      <input type="text" />
-      <button>Add Task</button>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Task</button>
       <button>Clear Finished Task</button>
       <div>0 left todo</div>
     </>
